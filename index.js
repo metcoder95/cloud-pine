e43c2199901a1b8c60654d840aa69762e1ca960c
Carlos Fuentes <me@metcoder.dev>
2022-07-21 23:31:16 +0200

    feat!: support for transport

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 'use strict'
+const split = require('split2')
+const { CloudLogging } = require('./lib/cloud-logging')
+
+async function CloudPine (options) {
+  const { logName = 'Cloud_Pine', cloudLoggingOptions } = options
+  const logging = new CloudLogging(logName, cloudLoggingOptions)
+
+  await logging.init()
+
+  const stream = split(logging.parseLine.bind(logging), { autoDestroy: true })
+
+  return stream
+}
+
+module.exports = CloudPine
