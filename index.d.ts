31b2ff2bf1a9c135659d4615e7d8dcab237a478c
Carlos Fuentes <me@metcoder.dev>
2022-08-16 23:04:44 +0200

    feat: add types

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,23 @@
 /// <reference types="node" />
+import { Transform } from 'stream';
+import { LoggingOptions, Log, LogSync } from '@google-cloud/logging';
+
+declare function CloudPine(options?: PrettyOptions_): Transform;
+
+type CloudPineOptions = {
+  logName?: string;
+  cloudLoggingOptions: {
+    googleCloudOptions?: LoggingOptions;
+    resourceSettings?: {
+      type?: string;
+      labels: Record<string, string>;
+    };
+    defaultLabels?: Record<string, string>;
+    skipInit?: boolean;
+    sync?: boolean;
+    logOptions?: ConstructorParameters<Log> | ConstructorParameters<LogSync>;
+  };
+};
+
+export default CloudPine;
+export { CloudPineOptions };
