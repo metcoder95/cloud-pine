e15e9da899f54518cb8af82df096c4a656ac85dc
Carlos Fuentes <me@metcoder.dev>
2022-08-18 22:26:42 +0200

    test: add types testing (#9)

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,23 @@
 /// <reference types="node" />
+import { Transform } from 'stream';
+import { LoggingOptions, Log, LogSync } from '@google-cloud/logging';
+
+declare function CloudPine(options?: CloudPineOptions): Transform;
+
+type CloudPineOptions = {
+  logName?: string;
+  cloudLoggingOptions: {
+    googleCloudOptions?: LoggingOptions;
+    resourceSettings?: {
+      type?: string;
+      labels: Record<string, string>;
+    };
+    defaultLabels?: Record<string, string>;
+    skipInit?: boolean;
+    sync?: boolean;
+    logOptions?: ConstructorParameters<typeof Log> | ConstructorParameters<typeof LogSync>;
+  };
+};
+
+export default CloudPine;
+export { CloudPineOptions };
